3d9e46cd376ccf93ecf1a480ea860e8243a28e7c
Jawadi Rami <67319552+Ramijawadi@users.noreply.github.com>
2025-07-22 11:58:10 +0100

    add about and contact pages

---
+++ my_app/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h2>About me </h2>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, provident! At cum illum expedita, soluta consequatur quod aliquid eius aspernatur, eligendi distinctio, id dolorum inventore quos! Nulla explicabo eligendi minima?</div>
+    </>
+  )
+}
+
+export default page
